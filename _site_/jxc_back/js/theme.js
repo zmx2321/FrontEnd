d2f01159a2e0781fb5da1fa841a5b79ccfe5f22f
zmx2321@163.com <zmx2321@163.com>
2018-08-08 17:08:13 +0800

    inn

--- _site_/jxc_back/js/theme.js
+++ _site_/jxc_back/js/theme.js
@@ -2,6 +2,49 @@ themeUtil = {
 
 }
 
+//nav置顶
+var setnavscroll = function(){
+    util.setnavscroll($('.navbar'), $('.pro_nav'), 'navfix');
+}
+
+//选项卡
+var proTab = function(){
+    /**
+     * swiper高度自适应
+     * [mirAutoHeight swiper高度自适应]
+     * @param  {[type]} i       [索引]
+     * @param  {[type]} mribtn  [选项卡按钮]
+     * @param  {[type]} tabitm  [内容主体部分]
+     * @param  {[type]} slide   [内容主体外部盒子]
+     * @param  {[type]} swipwap [swiper盒子]
+     * @param  {[type]} swipcot [swiper主体部分]
+     * @return {[type]}         [description]
+     */
+    function mirAutoHeight(i, mribtn, tabitm, slide, swipwap, swipcot){
+        mribtn.removeClass('active').eq(i).addClass('active');
+
+        var tabitm = tabitm.eq(i);
+
+        swipwap.css("height", tabitm.height());  //swiper-wrapper高度
+        swipcot.css("height", tabitm.height());  //swiper-container高度
+    }
+
+    // 滑动
+    var mrifoTabSwiper = new Swiper('.pro_cot', {
+        onTransitionEnd: function (swiper) {
+            mirAutoHeight(mrifoTabSwiper.activeIndex, $('#proTab li'), $(".pro_itm"), $(".pro_wap .swiper-slide"), 
+                $(".pro_wap"), $(".pro_cot"));
+        }
+    })
+    
+    // 列表切换
+    $('#proTab li').on('click', function (e) {
+        mirAutoHeight($(this).index(), $('#proTab li'), $(".pro_itm"), $(".pro_wap .swiper-slide"), 
+                $(".pro_wap"), $(".pro_cot"));
+        mrifoTabSwiper.slideTo($(this).index(), 200, false);
+    });
+}
+
 // 提交图文详情 (富文本 wangEditor)
 var submitEditor = function (){
     var E = window.wangEditor
@@ -35,7 +78,13 @@ var submitEditor = function (){
     });   
 }
 
+//滚动条滚动的时候加载
+$(window).scroll(function(){
+    setnavscroll();  //nav置顶
+});
+
 //load
 $(function(){
 	submitEditor();  // 提交图文详情 (富文本 wangEditor)
+    proTab();  //选项卡
 });
