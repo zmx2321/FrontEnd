fef0e22c77dfebfae797af3e9d4d836e81600015
zmx2321 <zmx2321@163.com>
2018-11-26 11:32:10 +0800

    init

--- vue/simple/eg/eg3/js/index.js
+++ vue/simple/eg/eg3/js/index.js
@@ -6,17 +6,31 @@ window.onload = function(){
 
         //初始数据
         data: {
-            
+            title: "vue 模板文件",
+            skstyle: "text-red",
+            skstyle2: "border",
+            isshow: true,
+            //定义地址
+            url: "http://img95.699pic.com/",
         },
 
         //预处理数据
         computed: {
-
-        }
+            //预处理图片（定义图片）
+            skpic: function(){
+                console.log(this.url);
+                return this.url + "element/40038/9894.png_300.png";
+            },
+        },
 
         //函数（事件）
         methods: {
             
+        },
+
+        //监听
+        watch: {
+
         }
     });
 }
