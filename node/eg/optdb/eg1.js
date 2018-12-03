616ca67d813d64d7b6a0eb8e1008b4d77869b86a
zmx2321 <zmx2321@163.com>
2018-12-04 06:11:47 +0800

    inti

--- node/eg/optdb/eg1.js
+++ node/eg/optdb/eg1.js
@@ -1,1 +1,17 @@
-console.log("aaa");
+//数据库配置npm install express body-parser mongoose --save
+
+//引入mongo模块
+const mogo = require("mongoose");
+
+//引入http
+const http = require(http);
+
+//协议
+const url = "mongodb://localhost:27017/mydb";
+
+//数据库连接
+mongo.connect(url, function (err, db) {
+    if(err) throw err;
+
+    console.log("数据库已经连接");
+});
