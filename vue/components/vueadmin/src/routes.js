a4c4d84c4a00a8e0628f6fedcf5b19bc7ed6ff23
zmx <zmx2321@163.com>
2018-12-14 18:14:09 +0800

    init

---
+++ vue/components/vueadmin/src/routes.js
@@ -0,0 +1,143 @@
+//一级
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import NotFound from './views/404.vue'
+//manage
+import DeviceManage from './views/manage/device_manage.vue'
+import DataManage from './views/manage/data_manage.vue'
+import OperateManage from './views/manage/operate_manage.vue'
+import AccountManage from './views/manage/account_manage.vue'
+
+// import Test from './views/test.vue'
+
+import Main from './views/Main.vue'
+import Table from './views/nav1/Table.vue'
+import Form from './views/nav1/Form.vue'
+import user from './views/nav1/user.vue'
+
+
+// import Page4 from './views/nav2/Page4.vue'
+// import Page5 from './views/nav2/Page5.vue'
+// import Page6 from './views/nav3/Page6.vue'
+// import echarts from './views/charts/echarts.vue'
+
+let routes = [
+    {
+        path: '/login',
+        component: Login,
+        name: '',
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: NotFound,
+        name: '',
+        hidden: true
+    },
+    //设备管理(device manage)
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'el-icon-setting',//图标样式class
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/device_manage', component: DeviceManage, name: '设备管理' }
+        ]
+    },
+    //数据管理(data manage)
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-bar-chart',//图标样式class
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/data_manage', component: DataManage, name: '数据管理' }
+        ]
+    },
+    //运营管理(operate manage)
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-id-card-o',//图标样式class
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/operate_manage', component: OperateManage, name: '运营管理' }
+        ]
+    },
+    //账号管理(account manage)
+    {
+        path: '/',
+        component: Home,
+        name: '',
+        iconCls: 'fa fa-address-card',//图标样式class
+        leaf: true,//只有一个节点
+        children: [
+            { path: '/account_manage', component: AccountManage, name: '账号管理' }
+        ]
+    },
+
+    //test
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: '',
+    //     iconCls: 'fa fa-address-card',//图标样式class
+    //     leaf: true,//只有一个节点
+    //     children: [
+    //         { path: '/test', component: Test, name: 'test' }
+    //     ]
+    // },
+
+    //{ path: '/main', component: Main },
+    {
+        path: '/',
+        component: Home,
+        name: '导航一',
+        iconCls: 'el-icon-message',//图标样式class
+        children: [
+            { path: '/main', component: Main, name: '主页', hidden: true },
+            { path: '/table', component: Table, name: 'Table' },
+            { path: '/form', component: Form, name: 'Form' },
+            { path: '/user', component: user, name: '列表' },
+        ]
+    },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: '导航二',
+    //     iconCls: 'fa fa-id-card-o',
+    //     children: [
+    //         { path: '/page4', component: Page4, name: '页面4' },
+    //         { path: '/page5', component: Page5, name: '页面5' }
+    //     ]
+    // },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: '',
+    //     iconCls: 'fa fa-address-card',
+    //     leaf: true,//只有一个节点
+    //     children: [
+    //         { path: '/page6', component: Page6, name: '导航三' }
+    //     ]
+    // },
+    // {
+    //     path: '/',
+    //     component: Home,
+    //     name: 'Charts',
+    //     iconCls: 'fa fa-bar-chart',
+    //     children: [
+    //         { path: '/echarts', component: echarts, name: 'echarts' }
+    //     ]
+    // },
+    {
+        path: '*',
+        hidden: true,
+        redirect: { path: '/404' }
+    }
+];
+
+export default routes;
---
+++ vue/components/vueadmin/src/views/manage/device_manage.vue
@@ -0,0 +1,80 @@
+<template>
+    <section>
+        <h2>设备管理</h2>
+
+        <!--添加新的设备
+        编辑位置信息
+        显示设备的存取餐二维码
+        查看设备状态
+        设备通信状态
+        设备柜口状态
+        远程开柜
+        开启单个特定柜口
+        一键开启所有柜口
+        重启设备-->
+
+
+        <el-button type="primary" @click="handleAdd">新增</el-button>
+        <el-button type="primary" @click="handleAdd2">新增2</el-button>
+
+        <!--<el-button size="small" @click="handleEdit">编辑</el-button>-->
+
+
+        <!--编辑界面-->
+        <!--<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
+
+        </el-dialog>
+
+        <!--新增界面-->
+        <el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+
+        </el-dialog>
+
+        <el-dialog title="新增2" v-model="addFormVisible2" :close-on-click-modal="false">
+
+        </el-dialog>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "device",
+
+        data() {
+            return {
+                filters: {
+                    name: ''
+                },
+
+                // editFormVisible: false,//编辑界面是否显示
+                // editLoading: false,
+
+                addFormVisible: false,//新增界面是否显示
+                addFormVisible2: false,//新增界面是否显示
+
+                addLoading: false,
+            }
+        },
+        methods: {
+            //显示编辑界面
+            // handleEdit: function () {
+            //     this.editFormVisible = true;
+            // },
+            //显示新增界面
+            handleAdd: function () {
+                this.addFormVisible = true;
+            },
+            handleAdd2: function () {
+                this.addFormVisible2 = true;
+            },
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
