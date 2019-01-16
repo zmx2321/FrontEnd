16a43187404df147ce5c544b44fbcee78477aa80
zmx <zmx2321@163.com>
2019-01-16 11:39:38 +0800

    init

--- vue/components/element-ui/project/node-vue-ele-app/routers/api/profiles.js
+++ vue/components/element-ui/project/node-vue-ele-app/routers/api/profiles.js
@@ -22,7 +22,28 @@ router
      */
     .get('/test', (req, res) => {
         res.json({ msg: 'profile works' });
-    });
+    })
+
+    /**
+     *  @route  POST api/profiles/add
+     *  @desc   创建信息接口
+     *  @access private(需要验证token接口)
+     */
+    .post('/add', passwort.authenticate("jwt", {session: false}), (req, res) => {
+        const profileFields = {};
+
+        if (req.body.type) profileFields.type = req.body.type;
+        if (req.body.describe) profileFields.describe = req.body.describe;
+        if (req.body.income) profileFields.income = req.body.income;
+        if (req.body.expend) profileFields.expend = req.body.expend;
+        if (req.body.cash) profileFields.cash = req.body.cash;
+        if (req.body.remark) profileFields.remark = req.body.remark;
+
+        // 如果存储成功
+        new Profile(profileFields).save().then(profile => {
+            res.json(profile);
+        });
+    })
 
 
 //对外提供一个接口
